099dede14b60d3ed2148db5c14a0c371a1eca0f0
Drwalin <marek.zalewski.drwalin@gmail.com>
2024-05-09 23:20:58 +0200

    Fix: function can be reported multiple times from single stack frame.
    Prevent from reporting function as sambled multiple times from  single stack
    frame. Thanks to this, functions are never reported to be over 100% of
    occurences nor sum of fully exclusive functions won't over 100%.

--- wallClockProfiler.cpp
+++ wallClockProfiler.cpp
@@ -784,6 +784,18 @@ int main(int inNumArgs, char **inArgs)
 			char *funcName = s->frames.getElement(f)->funcName;
 
 			char found = false;
+			for (int fdup = 0; fdup < f; fdup++) {
+				char *dupFuncName = s->frames.getElement(fdup)->funcName;
+				if (strcmp(funcName, dupFuncName) == 0) {
+					found = true;
+					break;
+				}
+			}
+			if (found) {
+				continue;
+			}
+
+			found = false;
 			for (int r = 0; r < functions.size(); r++) {
 				if (strcmp(functions.getElement(r)->funcName, funcName) == 0) {
 					// hit
